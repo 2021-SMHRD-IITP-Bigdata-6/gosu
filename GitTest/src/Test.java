f98271ce109e512943b36202e1156c5d2722c862
smhrd <smhrd@DESKTOP-BC2NFCA>
2021-12-02 16:33:34 +0900

    ㅁㄴㅇㄻㄴㅇㄹ

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -8,7 +8,6 @@ public class Test {
 		// 3. Encoding 방식(UTF-8)
 		// 이 내용은 팀 내 한분만 작성해주세요
 		// 혹시 충돌을 경험해보고 싶으시다면 작성해보세요!
-<<<<<<< HEAD
 		System.out.println("안녕하냐 맨이야");
 		System.out.println("테스트2");
 		System.out.println("ㅁㄴㅇㄹㄴㅁㅇㄹ");
@@ -17,12 +16,10 @@ public class Test {
 		System.out.println("응응");
 		System.out.println("응응");
 		
-=======
 	
 		System.out.println("dsfsdf");
 		System.out.println("어렵습니다.");
 		System.out.println("복잡 복잡");
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-IITP-Bigdata-6/gosu.git
 	}//main
 
 }//Test
