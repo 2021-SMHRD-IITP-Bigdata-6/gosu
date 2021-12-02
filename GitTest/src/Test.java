c7868e56e5012c00662d63b20077ca2da6586820
smhrd <smhrd@DESKTOP-N0C72HJ>
2021-12-02 16:14:49 +0900

    zz

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -8,10 +8,8 @@ public class Test {
 		// 3. Encoding 방식(UTF-8)
 		// 이 내용은 팀 내 한분만 작성해주세요
 		// 혹시 충돌을 경험해보고 싶으시다면 작성해보세요!
-		System.out.println("안녕하냐 맨이야");
-		System.out.println("테스트2");
-		System.out.println("ㅁㄴㅇㄹㄴㅁㅇㄹ");
-		System.out.println("ㄴㅇㄹㄴㅇㄹ");
+	
+		
 		System.out.println("ㅋㅋㅋ");
 	}//main
 
