0f7c25517b7367a47abac1f44d585a89f6bfcd4a
smhrd <smhrd@DESKTOP-BC2NFCA>
2021-12-02 15:46:21 +0900

    환경구축 설명추가 경로 GitTest -> 마오 -> Team -> Commit

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -2,7 +2,14 @@
 public class Test {
 
 	public static void main(String[] args) {
+		// Github 환경설정
+		// 1. JDK 버전
+		// 2. Tomcat 버전
+		// 3. Encoding 방식(UTF-8)
+		// 이 내용은 팀 내 한분만 작성해주세요
+		// 혹시 충돌을 경험해보고 싶으시다면 작성해보세요!
 		System.out.println("안녕하냐 맨이야");
+		System.out.println("테스트2");
 	}//main
 
 }//Test
