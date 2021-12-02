d616470b7b651dba7dbce26a660f59500ae02722
smhrd <smhrd@DESKTOP-09DG39B>
2021-12-02 16:37:23 +0900

    ㅁㄴㅇㅁㄴㅇ

--- GitTest/src/Test.java
+++ GitTest/src/Test.java
@@ -7,14 +7,8 @@ public class Test {
 		// 2. Tomcat 버전
 		// 3. Encoding 방식(UTF-8)
 		// 이 내용은 팀 내 한분만 작성해주세요
-<<<<<<< HEAD
 		// 혹시 충돌을 경험해보고 싶으시다면 작성해보세요!
-<<<<<<< HEAD
 
-=======
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-IITP-Bigdata-6/gosu.git
-=======
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-IITP-Bigdata-6/gosu.git
 		System.out.println("안녕하냐 맨이야");
 		System.out.println("테스트2");
 		System.out.println("ㅁㄴㅇㄹㄴㅁㅇㄹ");
@@ -22,15 +16,10 @@ public class Test {
 		System.out.println("ㅋㅋㅋ");
 		System.out.println("응응");
 		System.out.println("응응");
-<<<<<<< HEAD
+		System.out.println("응응");
 		
-<<<<<<< HEAD
-
-=======
 	
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-IITP-Bigdata-6/gosu.git
-=======
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-IITP-Bigdata-6/gosu.git
+	
 		System.out.println("dsfsdf");
 		System.out.println("어렵습니다.");
 		System.out.println("asdf sadf ");
