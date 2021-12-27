128179e712fcea167c812579f596ab3483b47c52
smhrd <smhrd@DESKTOP-N0C72HJ>
2021-12-27 09:29:37 +0900

    wd

--- testtt/src/com/book/DAO/BookDAO.java
+++ testtt/src/com/book/DAO/BookDAO.java
@@ -7,7 +7,6 @@ import java.sql.ResultSet;
 
 import com.book.DTO.BookDTO;
 
-
 public class BookDAO {
 	Connection conn = null;
 	PreparedStatement psmt = null;
@@ -19,38 +18,26 @@ public class BookDAO {
 	private boolean check;
 
 	public void getConn() {
+
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-<<<<<<< HEAD
-			System.out.println("클래스 파일 로딩 완료");
-			
-		
-			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524";
-			String dbid = "cgi_8_3_1216";
-			String dbpw = "smhrd3";
-			
-			conn = DriverManager.getConnection(url,dbid,dbpw);
-			
-			if(conn!=null) {
-=======
 			System.out.println("클래스파일 로딩완료");
 
 			// 3. DB에서 사용하는 id/pw를 인증
-			
 			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524";
-	         String dbid = "cgi_8_3_1216";
-	         String dbpw = "smhrd3";
+			String dbid = "cgi_8_3_1216";
+			String dbpw = "smhrd3";
 			conn = DriverManager.getConnection(url, dbid, dbpw);
 			if (conn != null) {
->>>>>>> branch 'master' of https://github.com/2021-SMHRD-IITP-Bigdata-6/gosu.git
 				System.out.println("연결성공");
-			}else {
+			} else {
 				System.out.println("연결실패");
 			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	}//getConn()
+	}
 
 	public void close() {
 		System.out.println("무조건실행");
@@ -69,6 +56,7 @@ public class BookDAO {
 			e.printStackTrace();
 		}
 	}
+
 	public BookDTO Login(BookDTO dto1) {
 
 		try {
@@ -76,7 +64,7 @@ public class BookDAO {
 			String sql = "select * from t_member where mem_id = ?";
 			// 5. SQL명령문을 준비
 			psmt = conn.prepareStatement(sql);
-			
+
 			psmt.setString(1, dto1.getMem_id());
 
 			// 6. slq명령문 실행
@@ -85,22 +73,18 @@ public class BookDAO {
 			// 7. 명령 후 처리
 			if (rs.next()) {
 
-			
-				
-				String getpw= rs.getString(2);
-				String getname= rs.getString(3);
-				String gettel= rs.getString(4);
-				int getage= rs.getInt(5);
-				String getgender= rs.getString(6);
-				String date= rs.getString(7);
-				
-				
+				String getpw = rs.getString(2);
+				String getname = rs.getString(3);
+				String gettel = rs.getString(4);
+				int getage = rs.getInt(5);
+				String getgender = rs.getString(6);
+				String date = rs.getString(7);
+
 				System.out.println(dto1.getMem_id());
 				System.out.print(getpw);
-			
-				
+
 				if (dto1.getMem_pw().equals(getpw)) {
-					
+
 					dto = new BookDTO(dto1.getMem_id(), getpw, getname, gettel, getage, getgender, date);
 
 				}
@@ -130,7 +114,7 @@ public class BookDAO {
 			psmt.setString(4, dto.getMem_tel());
 			psmt.setInt(5, dto.getMem_age());
 			psmt.setString(6, dto.getMem_gender());
-			
+
 			// 6. slq명령문 실행
 			cnt = psmt.executeUpdate();
 
@@ -146,5 +130,3 @@ public class BookDAO {
 	}
 
 }
-
-
